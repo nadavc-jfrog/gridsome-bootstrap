520af0f5be86a10f962ad09ae5dcad9feaf0da1d
Nadav Cohen <nadavc@jfrog.com>
2021-11-17 11:48:26 +0200

    first commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,25 @@
+<template>
+  <Layout>
+    
+    hello
+
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Gridsome starter bootstrap'
+  }
+}
+</script>
+
+<style>
+.greet-image {
+  display: block;
+  margin: auto;
+  width: 90%;
+  max-width: 500px;
+  padding-bottom: 50px;
+}
+</style>
