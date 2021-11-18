bb4beb6a9b6f5a0dd47417daaf019456a6eef717
Nadav Cohen <nadavc@jfrog.com>
2021-11-18 11:04:03 +0200

    robots

--- src/main.js
+++ src/main.js
@@ -8,6 +8,17 @@ import checkIfMobile from './mixins/checkIfMobile'
 import VueYouTubeEmbed from 'vue-youtube-embed'
 
 export default function (Vue, { router, head, isClient }) {
+  
+  //todo remove when published.
+  head.meta.push({
+    name: 'robots',
+    content: 'noindex'
+  })
+  head.meta.push({
+    name: 'googlebot',
+    content: 'noindex'
+  })
+
   Vue.use(BootstrapVue)
   Vue.use(VueYouTubeEmbed)
   Vue.component('Layout', DefaultLayout)
